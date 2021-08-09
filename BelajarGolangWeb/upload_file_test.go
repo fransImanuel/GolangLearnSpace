5a63ff029780bd6eb5b7f38aa821a0c40ea21348
fransImanuel <fransimanuel99@gmail.com>
2021-08-09 15:37:17 +0700

    finished go-lang web

---
+++ BelajarGolangWeb/upload_file_test.go
@@ -0,0 +1,84 @@
+package belajargolangweb
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func UploadForm(writer http.ResponseWriter, request *http.Request) {
+	myTemplates.ExecuteTemplate(writer, "upload.form.gohtml", nil)
+}
+
+func Upload(writer http.ResponseWriter, request *http.Request) {
+	// request.ParseMultipartForm(32 << 20)
+	//get file from user input
+	file, fileHeader, err := request.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	// create file destination
+	fileDestination, err := os.Create("./resources/" + fileHeader.Filename)
+	if err != nil {
+		panic(err)
+	}
+	//copy file from user input to file destination
+	_, err = io.Copy(fileDestination, file)
+	if err != nil {
+		panic(err)
+	}
+	name := request.PostFormValue("name")
+	myTemplates.ExecuteTemplate(writer, "upload.success.gohtml", map[string]interface{}{
+		"Name": name,
+		"File": "/static/" + fileHeader.Filename,
+	})
+
+}
+
+func TestUploadForm(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", UploadForm)
+	mux.HandleFunc("/upload", Upload)
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./resources"))))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/unknown.png
+var uploadFileTest []byte
+
+func TestUploadFile(t *testing.T) {
+	body := new(bytes.Buffer)
+
+	writer := multipart.NewWriter(body)
+	writer.WriteField("name", "frans IMANUEL")
+	file, err := writer.CreateFormFile("file", "CONTOHUPLOAD.png")
+	if err != nil {
+		panic(err)
+	}
+	file.Write(uploadFileTest)
+	writer.Close()
+
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/upload", body)
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+	recorder := httptest.NewRecorder()
+
+	Upload(recorder, request)
+
+	bodyResponse, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(bodyResponse))
+}
